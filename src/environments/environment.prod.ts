f013a7b25925e1074ce20b69b650944a6420ec77
Luana Fernandes <lumayara.ads@gmail.com>
2020-12-01 15:30:40 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  create: 'https://us-central1-dictionary-db8cd.cloudfunctions.net/app/api/create',
+  readId: 'https://us-central1-dictionary-db8cd.cloudfunctions.net/app/api/read/',
+  readAll: 'https://us-central1-dictionary-db8cd.cloudfunctions.net/app/api/read',
+  update: 'https://us-central1-dictionary-db8cd.cloudfunctions.net/app/api/update/',
+  delete: 'https://us-central1-dictionary-db8cd.cloudfunctions.net/app/api/delete/'
 };
